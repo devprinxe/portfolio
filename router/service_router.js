efdee685982a3abb7ece3d26cc7280d41d19fc61
princemahmud <prince.mahmud@grameen.technology>
2023-07-02 15:53:42 +0600

    Service router added

--- router/service_router.js
+++ router/service_router.js
@@ -1,11 +1,6 @@
 import express from "express";
 import { upload } from "../services/file_upload_service.js";
 import { saveFile, sendMail } from "../controller/service_controller.js";
-import {
-  fileUploadValidationRules,
-  validateFile,
-} from "../middleware/service_validation.js";
-
 const serviceRoutes = express.Router();
 
 serviceRoutes.post("/file-upload", upload.single("file"), saveFile);
